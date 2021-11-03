e666ddba49e0ccfd728458ce6f2b4b2cbc1c1a41
Sagal Afrah <afrahsagal@gmail.com>
2021-11-03 19:46:09 -0400

    input linted

--- input.js
+++ input.js
@@ -1,19 +1,44 @@
-const setupInput = function () {
-    const stdin = process.stdin;
-    stdin.setRawMode(true);
-    stdin.setEncoding("utf8");
-    stdin.resume();
-    stdin.on("data", handleUserInput);
-    return stdin;
+const {connect} = require("./client");
+let connection;
 
-    // stdin.on('data', (key) => {
-    //     process.stdout.write('.');
-    //   });
-  };
+
+const setupInput = function(conn) {
+  connection = conn;
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
   
-const handleUserInput = function (key) {
-    if (key === '\u0003') {
-        process.exit();
-      }
-  };
-  exports.setupInput = setupInput;
+
+};
+  
+const handleUserInput = function(key) {
+  if (key === '\u0003') {
+    process.exit();
+  }
+  if (key === 'w') {
+    connection.write('Move: up');
+  }
+  if (key === 'a') {
+    connection.write('Move: left');
+  }
+  if (key === 's') {
+    connection.write('Move: down');
+  }
+  if (key === 'd') {
+    connection.write('Move: right');
+  }
+  if (key === 'x') {
+    connection.write('Say: <3');
+  }
+
+
+  
+};
+exports.setupInput = setupInput;
+
+
+
+ 
+    
