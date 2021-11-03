dd478edecfb580622c9474ddfe0f9224ad9f2fcf
Sagal Afrah <afrahsagal@gmail.com>
2021-11-03 15:54:34 -0400

    data edit

--- play.js
+++ play.js
@@ -6,8 +6,21 @@ const connect = function () {
     host: '165.227.47.243',
     port: 50541,
   });
+//   conn.on('end', () => {
+//     console.log('you ded cuz you idled');
+//   });
+//   return conn.on;
 
   // interpret incoming data as text
+
+  conn.on('data', (data) => {
+    console.log(data.toString());
+    conn.end();
+  });
+  conn.on('end', () => {
+    console.log('you ded cuz you idled');
+  });
+
   conn.setEncoding("utf8");
 
   return conn;
